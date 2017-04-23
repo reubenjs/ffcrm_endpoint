f00155fc016c234f2f73cec405edb8a412585a1d
Reuben Salagaras <reuben.salagaras@gmail.com>
2017-04-23 21:04:39 +0930

    bubble up errors from process and return 500 if any occur

--- app/controllers/ffcrm_endpoint/endpoints_controller.rb
+++ app/controllers/ffcrm_endpoint/endpoints_controller.rb
@@ -5,10 +5,13 @@ module FfcrmEndpoint
     respond_to :json, :js, :html
 
     def consume
-
       if endpoint.authenticate
-        endpoint.process
-        respond_with('', location: nil, status: 201)
+        begin
+          endpoint.process
+          respond_with('', location: nil, status: 201)
+        rescue RuntimeError => e
+          respond_with(e.message, location: nil, status: 500)
+        end
       else
         respond_with('', location: nil, status: :unauthorized)
       end
